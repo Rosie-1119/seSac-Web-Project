121a048b1415229a85920526471316b15785cf65
seoyoung <im.your.violet@gmail.com>
2022-02-04 20:31:45 +0900

    힘이드니까 검색 컨트롤러 커밋좀...

--- fiveand-web-project/src/com/fiveand/search/service/SearchService.java
+++ fiveand-web-project/src/com/fiveand/search/service/SearchService.java
@@ -1,5 +1,22 @@
 package com.fiveand.search.service;
 
+import java.util.List;
+
+import com.fiveand.auction.board.vo.ProductVO;
+import com.fiveand.search.dao.SearchDAO;
+
 public class SearchService {
 
+	private SearchDAO searchDao;
+
+	public SearchService() {
+		searchDao = new SearchDAO();
+	}
+
+	public List<ProductVO> searchList(String findStr) {
+
+		List<ProductVO> list = searchDao.searchList(findStr);
+		System.out.println(list);
+		return list;
+	}
 }
